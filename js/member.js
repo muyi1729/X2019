6abef0fe92f6ae2e2b323157ebf48236b96ff719
muyi1729 <88604849+muyi1729@users.noreply.github.com>
2022-08-25 22:25:10 +0800

    Add files via upload

---
+++ js/member.js
@@ -0,0 +1,174 @@
+var member = [
+  {
+  	"phone": "No.01",
+    "name": "彭奕程"
+  },
+  {
+    "phone": "No.02",
+    "name": "高瑾宸"
+  },
+  {
+    "phone": "No.03",
+    "name": "刘静怡"
+  },
+  {
+    "phone": "No.04",
+    "name": "谭峻熙"
+  },
+  {
+    "phone": "No.04",
+    "name": "程奕希"
+  },
+  {
+    "phone": "No.04",
+    "name": "李奕姿"
+  },
+  {
+    "phone": "No.04",
+    "name": "刘思颖"
+  },
+  {
+    "phone": "No.04",
+    "name": "潘雅萱"
+  },
+  {
+    "phone": "No.04",
+    "name": "何亦晴"
+  },
+  {
+    "phone": "No.04",
+    "name": "樊馨怡"
+  },
+  {
+    "phone": "No.04",
+    "name": "周暄涵"
+  },
+  {
+    "phone": "No.04",
+    "name": "胡乐萱"
+  },
+  {
+    "phone": "No.04",
+    "name": "陈堇妍"
+  },
+  {
+    "phone": "No.04",
+    "name": "彭梦妍"
+  },
+  {
+    "phone": "No.04",
+    "name": "彭芸锦"
+  },
+  {
+    "phone": "No.04",
+    "name": "左佑"
+  },
+  {
+    "phone": "No.04",
+    "name": "彭睿祥"
+  },
+  {
+    "phone": "No.04",
+    "name": "熊逸轩"
+  },
+  {
+    "phone": "",
+    "name": "胡皓轩"
+  },
+  {
+    "phone": "",
+    "name": "盛晨轩"
+  },
+  {
+    "phone": "",
+    "name": "周嘉艺"
+  },
+  {
+    "phone": "",
+    "name": "夏晟铭"
+  },
+  {
+    "phone": "",
+    "name": "任孜睿"
+  },
+  {
+    "phone": "",
+    "name": "傅卓"
+  },
+  {
+    "phone": "",
+    "name": "孙一帆"
+  },
+  {
+    "phone": "",
+    "name": "杨子涵"
+  },
+  {
+    "phone": "",
+    "name": "赵宇麟"
+  },
+  {
+    "phone": "",
+    "name": "邓梓涵"
+  },
+  {
+    "phone": "",
+    "name": "陈惜睿"
+  },
+  {
+    "phone": "",
+    "name": "程泽语"
+  },
+  {
+    "phone": "",
+    "name": "罗昊晨"
+  },
+  {
+    "phone": "",
+    "name": "刘维桢"
+  },
+  {
+    "phone": "",
+    "name": "田紫睿"
+  },
+  {
+    "phone": "",
+    "name": "陈诗文"
+  },
+  {
+    "phone": "",
+    "name": "龚宇轩"
+  },
+  {
+    "phone": "",
+    "name": "张雅茜"
+  },
+  {
+    "phone": "",
+    "name": "何梓轩"
+  },
+  {
+    "phone": "",
+    "name": "谢珍琦"
+  },
+  {
+    "phone": "",
+    "name": "陈雨瞳"
+  },
+  {
+    "phone": "",
+    "name": "高熙沛"
+  },
+  {
+    "phone": "",
+    "name": "王悦灵"
+  },
+  {
+    "phone": "",
+    "name": "庞海澜"
+  },
+  {
+    "phone": "",
+    "name": "侯睿霖"
+  }
+]
